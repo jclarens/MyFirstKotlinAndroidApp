9e50d0ea979fd69dfa9f84d9e4dc9dede4c1e0de
JClarens <Clarens.Jay@gmail.com>
2018-10-13 00:25:22 +0700

    add resetGame

--- Time Fighter/app/src/main/java/com/example/jclarens/timefighter/MainActivity.kt
+++ Time Fighter/app/src/main/java/com/example/jclarens/timefighter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.jclarens.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
     internal var score = 0
+    internal var gameStarted = false
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown : Long = 60000
+    internal val countDownInterval: Long = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,11 +25,21 @@ class MainActivity : AppCompatActivity() {
         gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
         timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
 
+//      gameScoreTextView.text = getString(R.string.your_score,score.toString())
+        val a = getString(R.string.your_score,score.toString())
+        gameScoreTextView.setText(a)
+
         tapMeButton.setOnClickListener { view ->
             incrementScore()
         }
     }
+        fun resetGame(){
+            score = 0
+            gameScoreTextView.setText(getString(R.string.your_score,score.toString()))
 
+            val initialTimeLeft = initialCountDown / 1000
+            timeLeftTextView.setText(getString(R.string.time_left,initialTimeLeft.toString()))
+        }
     private fun incrementScore() {
         score = score+1
 
